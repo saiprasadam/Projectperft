f42be5b425b77f0bde22c8e35f51d13cef4f250b
saiprasadam <32019169+saiprasadam@users.noreply.github.com>
2020-05-12 12:08:08 +0530

    Add files via upload

---
+++ Testjav1/src/com/test/proj/SampleJbean.java
@@ -0,0 +1,16 @@
+package com.test.proj;
+
+public class SampleJbean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a=10,b=5;
+		float c=a+b;
+	
+System.out.println("display add "+a+b);
+System.out.println("display value C "+c);
+System.out.println("display add "+(a-b));
+	}
+
+}
